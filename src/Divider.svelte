574081714dfe62e0bb09690a33b46861303ec9f0
rizzzse <rizzzse@gmail.com>
2021-11-16 18:40:02 +0900

    add <Divider />

---
+++ src/Divider.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export const left = "10%";
+</script>
+
+<div style="--left: {left};">
+    <span>
+        <slot></slot>
+    </span>
+</div>
+
+<style>
+    div {
+        display: flex;
+    }
+    div::before, div::after {
+        content: "";
+        border-top: 1px solid black;
+        transform: translateY(50%);
+    }
+    div::before { width: var(--left); }
+    div::after { width: calc(100% - var(--left)); }
+    span {
+        padding-left: 1em;
+        padding-right: 1em;
+    }
+</style>
